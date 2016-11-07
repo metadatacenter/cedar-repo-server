fcdd7e9a247d5866c80f0f37bfc0a3ffc2889780
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2016-11-07 10:03:40 -0800

    Static imports added

--- cedar-repo-server-core/src/main/java/org/metadatacenter/cedar/resource/util/ProxyUtil.java
+++ cedar-repo-server-core/src/main/java/org/metadatacenter/cedar/resource/util/ProxyUtil.java
@@ -3,15 +3,17 @@ package org.metadatacenter.cedar.resource.util;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.apache.http.Header;
 import org.apache.http.HeaderIterator;
-import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.entity.ContentType;
-import org.metadatacenter.constant.HttpConnectionConstants;
 import org.metadatacenter.server.security.model.AuthRequest;
 import play.mvc.Http;
 
 import java.io.IOException;
+
+import static org.apache.http.HttpHeaders.*;
+import static org.metadatacenter.constant.HttpConnectionConstants.CONNECTION_TIMEOUT;
+import static org.metadatacenter.constant.HttpConnectionConstants.SOCKET_TIMEOUT;
 
 public class ProxyUtil {
 
@@ -19,27 +21,27 @@ public class ProxyUtil {
 
   public static HttpResponse proxyGet(String url, Http.Request request) throws IOException {
     Request proxyRequest = Request.Get(url)
-        .connectTimeout(HttpConnectionConstants.CONNECTION_TIMEOUT)
-        .socketTimeout(HttpConnectionConstants.SOCKET_TIMEOUT);
-    proxyRequest.addHeader(HttpHeaders.AUTHORIZATION, request.getHeader(HttpHeaders.AUTHORIZATION));
+        .connectTimeout(CONNECTION_TIMEOUT)
+        .socketTimeout(SOCKET_TIMEOUT);
+    proxyRequest.addHeader(AUTHORIZATION, request.getHeader(AUTHORIZATION));
     return proxyRequest.execute().returnResponse();
   }
 
   public static HttpResponse proxyGet(String url, AuthRequest authRequest) throws IOException {
     Request proxyRequest = Request.Get(url)
-        .connectTimeout(HttpConnectionConstants.CONNECTION_TIMEOUT)
-        .socketTimeout(HttpConnectionConstants.SOCKET_TIMEOUT);
-    proxyRequest.addHeader(HttpHeaders.AUTHORIZATION, authRequest.getAuthHeader());
+        .connectTimeout(CONNECTION_TIMEOUT)
+        .socketTimeout(SOCKET_TIMEOUT);
+    proxyRequest.addHeader(AUTHORIZATION, authRequest.getAuthHeader());
     return proxyRequest.execute().returnResponse();
   }
 
   public static HttpResponse proxyDelete(String url, Http.Request request) throws IOException {
     Request proxyRequest = Request.Delete(url)
-        .connectTimeout(HttpConnectionConstants.CONNECTION_TIMEOUT)
-        .socketTimeout(HttpConnectionConstants.SOCKET_TIMEOUT);
-    proxyRequest.addHeader(HttpHeaders.AUTHORIZATION, request.getHeader(HttpHeaders.AUTHORIZATION));
-    proxyRequest.addHeader(HttpHeaders.CONTENT_LENGTH, ZERO_LENGTH);
-    proxyRequest.addHeader(HttpHeaders.CONTENT_TYPE, ContentType.TEXT_PLAIN.toString());
+        .connectTimeout(CONNECTION_TIMEOUT)
+        .socketTimeout(SOCKET_TIMEOUT);
+    proxyRequest.addHeader(AUTHORIZATION, request.getHeader(AUTHORIZATION));
+    proxyRequest.addHeader(CONTENT_LENGTH, ZERO_LENGTH);
+    proxyRequest.addHeader(CONTENT_TYPE, ContentType.TEXT_PLAIN.toString());
     return proxyRequest.execute().returnResponse();
   }
 
@@ -55,9 +57,9 @@ public class ProxyUtil {
   public static HttpResponse proxyPost(String url, Http.Request request, String content) throws IOException {
     Request proxyRequest = Request.Post(url)
         .bodyString(content, ContentType.APPLICATION_JSON)
-        .connectTimeout(HttpConnectionConstants.CONNECTION_TIMEOUT)
-        .socketTimeout(HttpConnectionConstants.SOCKET_TIMEOUT);
-    proxyRequest.addHeader(HttpHeaders.AUTHORIZATION, request.getHeader(HttpHeaders.AUTHORIZATION));
+        .connectTimeout(CONNECTION_TIMEOUT)
+        .socketTimeout(SOCKET_TIMEOUT);
+    proxyRequest.addHeader(AUTHORIZATION, request.getHeader(AUTHORIZATION));
     return proxyRequest.execute().returnResponse();
   }
 
@@ -69,9 +71,9 @@ public class ProxyUtil {
   public static HttpResponse proxyPut(String url, Http.Request request, String content) throws IOException {
     Request proxyRequest = Request.Put(url)
         .bodyString(content, ContentType.APPLICATION_JSON)
-        .connectTimeout(HttpConnectionConstants.CONNECTION_TIMEOUT)
-        .socketTimeout(HttpConnectionConstants.SOCKET_TIMEOUT);
-    proxyRequest.addHeader(HttpHeaders.AUTHORIZATION, request.getHeader(HttpHeaders.AUTHORIZATION));
+        .connectTimeout(CONNECTION_TIMEOUT)
+        .socketTimeout(SOCKET_TIMEOUT);
+    proxyRequest.addHeader(AUTHORIZATION, request.getHeader(AUTHORIZATION));
     return proxyRequest.execute().returnResponse();
   }
 
@@ -79,7 +81,7 @@ public class ProxyUtil {
     HeaderIterator headerIterator = proxyResponse.headerIterator();
     while (headerIterator.hasNext()) {
       Header header = headerIterator.nextHeader();
-      if (HttpHeaders.CONTENT_TYPE.equals(header.getName())) {
+      if (CONTENT_TYPE.equals(header.getName())) {
         response.setHeader(header.getName(), header.getValue());
       }
     }
