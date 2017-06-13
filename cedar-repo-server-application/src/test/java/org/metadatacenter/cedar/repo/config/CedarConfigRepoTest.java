7e56113a0b5707d11cbd05cfe9d078f6e5193916
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-06-13 11:17:38 -0700

    ENV vars added to tests

--- cedar-repo-server-application/src/test/java/org/metadatacenter/cedar/repo/config/CedarConfigRepoTest.java
+++ cedar-repo-server-application/src/test/java/org/metadatacenter/cedar/repo/config/CedarConfigRepoTest.java
@@ -20,6 +20,8 @@ public class CedarConfigRepoTest {
 
     env.put(CedarEnvironmentVariable.CEDAR_HOST.getName(), "metadatacenter.orgx");
 
+    env.put(CedarEnvironmentVariable.CEDAR_NET_GATEWAY.getName(), "127.0.0.1");
+
     env.put(CedarEnvironmentVariable.CEDAR_MONGO_APP_USER_NAME.getName(), "cedarUser");
     env.put(CedarEnvironmentVariable.CEDAR_MONGO_APP_USER_PASSWORD.getName(), "password");
     env.put(CedarEnvironmentVariable.CEDAR_MONGO_HOST.getName(), "localhost");
@@ -28,6 +30,9 @@ public class CedarConfigRepoTest {
     env.put(CedarEnvironmentVariable.CEDAR_REPO_HTTP_PORT.getName(), "9002");
     env.put(CedarEnvironmentVariable.CEDAR_REPO_ADMIN_PORT.getName(), "9102");
     env.put(CedarEnvironmentVariable.CEDAR_REPO_STOP_PORT.getName(), "9202");
+
+    env.put(CedarEnvironmentVariable.CEDAR_FOLDER_HTTP_PORT.getName(), "9008");
+    env.put(CedarEnvironmentVariable.CEDAR_USER_HTTP_PORT.getName(), "9005");
 
     TestUtil.setEnv(env);
   }
